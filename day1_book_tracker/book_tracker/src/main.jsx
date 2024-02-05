175a86c2b184b08509988b2cb41154a9ba309e59
totycos <dv94@hotmail.fr>
2024-02-05 09:54:55 +0100

    Commit REACT day 1 - Init book_tracker project

---
+++ day1_book_tracker/book_tracker/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+
+const App = () => {
+
+
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('root')); 
